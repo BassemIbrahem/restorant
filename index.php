1dcc8579d42f3db8df0f853ad9a5d0b37f7f6ce6
bassemibrahemmohamed <bassemibrahemmohamed@yahoo.com>
2025-01-29 21:16:18 +0200

    most popular items section in index page

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $query = "select * from foods where meal_id='1'";    
+    $app = new App;
+    $meals_1 = $app->selectAll($query);
+
+    $query = "select * from foods where meal_id='2'";    
+    $app = new App;
+    $meals_2 = $app->selectAll($query);
+
+    $query = "select * from foods where meal_id='3'";    
+    $app = new App;
+    $meals_3 = $app->selectAll($query);
+
+
+
+?>
